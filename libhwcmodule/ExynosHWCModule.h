36d9702f96ce3b1dbc87db97395a4a211bb3804f
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-07 19:10:10 +0000

    [TEST] libhwcmodule: Disable SecureDMA for video playback
    * It's logical that since this doesn't work on 7420 it also won't work on 5433, but there's only one way to find out.
    
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- libhwcmodule/ExynosHWCModule.h
+++ libhwcmodule/ExynosHWCModule.h
@@ -43,6 +43,9 @@
 /* Max number windows available in Exynos7570 is 3. */
 #define NUM_AVAILABLE_HW_WINDOWS	3
 
+/* Disable Secure DMA registers */
+#define DISABLE_IDMA_SECURE
+
 #ifdef FIMD_BW_OVERLAP_CHECK
 const size_t MAX_NUM_FIMD_DMA_CH = 3;
 const uint32_t FIMD_DMA_CH_IDX[] = {0, 1, 2};
